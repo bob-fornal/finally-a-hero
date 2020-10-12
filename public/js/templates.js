7670ba82b4383a452cdbfdc6ed0323950094e8dd
Bob Fornal <bob.fornal@leadingedje.com>
2020-10-12 11:29:28 -0400

    Initial Menu and Footer CSS

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,21 +1,26 @@
 
 const templates = {};
 
-templates.init = (filename, elementIdentifier) => {
+templates.init = (filename, elementIdentifier, location = null) => {
   const templateLocation = `/templates/${ filename }`;
 
   fetch(templateLocation)
     .then(response => response.text())
-    .then(html => templates.placeHTML(html, elementIdentifier))
+    .then(html => templates.placeHTML(html, elementIdentifier, location))
     .catch(err => console.log('Failed to fetch page: ', err));
 };
 
-templates.placeHTML = (html, elementIdentifier) => {
+templates.placeHTML = (html, elementIdentifier, location) => {
   const parser = new DOMParser();
   const parsed = parser.parseFromString(html, 'text/html');
 
-  const content = parsed.querySelector('.import-content');
-  const location = document.querySelector(elementIdentifier);
+  if (location !== null) {
+    const menuItem = parsed.querySelector(`.${ location }`);
+    menuItem.classList.add('active');  
+  }
 
-  location.appendChild(content);
+  const content = parsed.querySelector('.import-content');
+  const domLocation = document.querySelector(elementIdentifier);
+
+  domLocation.appendChild(content);
 };
